165755a196bb1046341d7aaf7cb9d24503093944
bartr <bartr@microsoft.com>
2020-11-05 21:12:00 +0000

    moved app to sub directory

---
+++ src/ngsa/tests/BaseTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace tests
+{
+    public class BaseTest
+    {
+        protected static void ConstructorMustThrowArgumentNullException(Type type)
+        {
+            Assert.NotNull(type);
+
+            foreach (ConstructorInfo constructor in type.GetConstructors())
+            {
+                ParameterInfo[] parameters = constructor.GetParameters();
+                Mock[] mocks = parameters.Select(p =>
+                {
+                    Type mockType = typeof(Mock<>).MakeGenericType(p.ParameterType);
+                    return (Mock)Activator.CreateInstance(mockType);
+                }).ToArray();
+
+                for (int index = 0; index < parameters.Length; index++)
+                {
+                    object[] mocksCopy = mocks.Select(m => m.Object).ToArray();
+                    mocksCopy[index] = null;
+
+                    string message = parameters[index].Name;
+                    try
+                    {
+                        Assert.Throws<ArgumentNullException>(() =>
+                        {
+                            constructor.Invoke(mocksCopy);
+                        });
+                    }
+                    catch (TargetInvocationException targetInvocationException)
+                    {
+                        targetInvocationException.InnerException.Should().BeOfType<ArgumentNullException>();
+                        targetInvocationException.InnerException.Message.Should().Contain(message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/ngsa/tests/RunApp.cs
@@ -0,0 +1,42 @@
+using System;
+using System.CommandLine;
+using System.Threading.Tasks;
+using CSE.NextGenSymmetricApp;
+using Xunit;
+
+namespace Tests
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1001:Types that own disposable fields should be disposable")]
+    public class AppTest
+    {
+        [Fact]
+        public async Task RunApp()
+        {
+            string[] args;
+
+            // test command line parser
+            RootCommand root = App.BuildRootCommand();
+
+            Assert.Equal(0, root.Parse("-d").Errors.Count);
+            Assert.Equal(0, root.Parse("-l Error").Errors.Count);
+            Assert.Equal(0, root.Parse("-l None").Errors.Count);
+            Assert.Equal(0, root.Parse("--secrets-volume foo").Errors.Count);
+
+            Assert.Equal(1, root.Parse("-foo").Errors.Count);
+            Assert.Equal(2, root.Parse("-foo bar").Errors.Count);
+
+            args = new string[] { "-l", "Warning", "--help", "-d", "--version" };
+            Assert.Equal(0, await App.Main(args));
+
+            args = new string[] { "--secrets-volume", "foo" };
+            Assert.Equal(-1, await App.Main(args));
+
+            if (!string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("RUN_TEST_COVERAGE")))
+            {
+                Console.WriteLine("Starting web server");
+                App.Main(Array.Empty<string>()).Wait(20000);
+                Console.WriteLine("Web server stopped");
+            }
+        }
+    }
+}
