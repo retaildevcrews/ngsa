a75380053425b16b31e3b3581dcfa9675ef7605a
bartr <bartr@microsoft.com>
2020-12-13 13:03:00 -0600

    moved, renamed, refactored

---
+++ spikes/data/ngsa-csharp/Ngsa.App/Controllers/Validation/ValidationDetail.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Ngsa.App.Validation
+{
+    /// <summary>
+    /// Model class for Validation Problem Details
+    /// </summary>
+    public class ValidationDetail
+    {
+        private readonly List<ValidationError> validationErrors = new List<ValidationError>();
+
+        public string Type { get; set; }
+
+        public string Title { get; set; }
+
+        public string Detail { get; set; }
+
+        public int Status { get; set; }
+
+        public string Instance { get; set; }
+
+        public ICollection<ValidationError> ValidationErrors => validationErrors;
+    }
+}
