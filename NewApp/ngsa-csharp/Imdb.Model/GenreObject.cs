c4f5ae41b1295c7f0ae938ee98645c6ea19fdfc3
Bart Robertson <bartr@microsoft.com>
2020-12-14 16:51:31 -0600

    fixed merge error (#246)

--- NewApp/ngsa-csharp/Imdb.Model/GenreObject.cs
+++ NewApp/ngsa-csharp/Imdb.Model/GenreObject.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-namespace CSE.NextGenSymmetricApp.Model
+namespace Imdb.Model
 {
     public class GenreObject
     {
