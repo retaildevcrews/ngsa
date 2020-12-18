dd6db4978978a2712ed6999cd5cb8119c85b0f79
bartr <bartr@microsoft.com>
2020-12-18 01:07:28 -0600

    added in-memory tests

--- NewApp/ngsa-csharp/Ngsa.DataService.Tests/AppTests.cs
+++ NewApp/ngsa-csharp/Ngsa.DataService.Tests/AppTests.cs
@@ -1,8 +1,13 @@
 using System;
 using System.CommandLine;
+using System.Linq;
 using System.Threading.Tasks;
 using Ngsa.DataService;
+using Ngsa.DataService.DataAccessLayer;
+using Ngsa.Middleware;
+using Imdb.Model;
 using Xunit;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -23,7 +28,7 @@ namespace Tests
             if (!string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("RUN_TEST_COVERAGE")))
             {
                 Console.WriteLine("Starting web server");
-                App.Main(Array.Empty<string>()).Wait(40000);
+                App.Main(Array.Empty<string>()).Wait(10000);
                 Console.WriteLine("Web server stopped");
             }
         }
@@ -43,6 +48,49 @@ namespace Tests
 
             Assert.Equal(1, root.Parse("--foo").Errors.Count);
             Assert.Equal(2, root.Parse("-f bar").Errors.Count);
+        }
+
+        [Fact]
+        public async Task InMemoryTests()
+        {
+            InMemoryDal dal = new InMemoryDal();
+
+            Movie m = await dal.GetMovieAsync("tt0133093").ConfigureAwait(false);
+            Assert.Equal("tt0133093", m.MovieId);
+
+            Actor a = await dal.GetActorAsync("nm0000031").ConfigureAwait(false);
+            Assert.Equal("nm0000031", a.ActorId);
+
+            var movies = await dal.GetMoviesAsync(new MovieQueryParameters { Genre = "Action" }).ConfigureAwait(false);
+            Assert.Equal(100, movies.ToList().Count);
+
+            movies = await dal.GetMoviesAsync(new MovieQueryParameters { Genre = "Action" }).ConfigureAwait(false);
+            Assert.Equal(100, movies.ToList().Count);
+
+            movies = await dal.GetMoviesAsync(new MovieQueryParameters { Year = 1999 }).ConfigureAwait(false);
+            Assert.Equal(39, movies.ToList().Count);
+
+            movies = await dal.GetMoviesAsync(new MovieQueryParameters { Rating = 8.5 }).ConfigureAwait(false);
+            Assert.Equal(20, movies.ToList().Count);
+
+            movies = await dal.GetMoviesAsync(new MovieQueryParameters { ActorId = "nm0000206" }).ConfigureAwait(false);
+            Assert.Equal(45, movies.ToList().Count);
+
+            movies = await dal.GetMoviesAsync(new MovieQueryParameters { Q = "ring" }).ConfigureAwait(false);
+            Assert.Equal(7, movies.ToList().Count);
+
+
+            MovieQueryParameters mp = new MovieQueryParameters
+            {
+                Year = 2000,
+                Genre = "Action",
+                Rating = 7,
+                Q = "Gladiator",
+                ActorId = "nm0000128",
+            };
+
+            movies = await dal.GetMoviesAsync(mp).ConfigureAwait(false);
+            Assert.Single(movies.ToList());
         }
     }
 }
