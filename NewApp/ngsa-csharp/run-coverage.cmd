668a4868245f7d0e8d8f9d58f375bbb5b5165253
bartr <bartr@microsoft.com>
2020-12-18 16:14:47 -0600

    updated coverage script

--- NewApp/ngsa-csharp/run-coverage.cmd
+++ NewApp/ngsa-csharp/run-coverage.cmd
@@ -2,25 +2,17 @@
 
 set RUN_TEST_COVERAGE=true
 dotnet tool install -g dotnet-reportgenerator-globaltool
+dotnet clean
 dotnet build
 cd Ngsa.LodeRunner
 
-start dotnet test ..\Ngsa.DataService.Tests\Ngsa.DataService.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=..\TestResults\
+start dotnet test ..\Ngsa.App.Tests\Ngsa.App.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=..\TestResults\ /p:MergeWith=..\TestResults\coverage.json
+timeout /T 10 /nobreak
 
-timeout /T 25 /nobreak
-
-dotnet run -- -s http://localhost:4122 -f dataservice.json
-
-timeout /T 30 /nobreak
-
-start dotnet run --project ..\Ngsa.Dataservice\Ngsa.DataService.csproj
-
-start dotnet test ..\Ngsa.App.Tests\Ngsa.App.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=../TestResults/ /p:MergeWith=../TestResults/coverage.json /p:CoverletOutputFormat=opencover
-
-timeout /T 20 /nobreak
+start dotnet test ..\Ngsa.DataService.Tests\Ngsa.DataService.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=..\TestResults\  /p:MergeWith=../TestResults/coverage.json /p:CoverletOutputFormat=opencover
+timeout /T 15 /nobreak
 
 dotnet run -- -s http://localhost:4120 -f baseline.json
-
 timeout /T 25 /nobreak
 
 cd ..\TestResults
