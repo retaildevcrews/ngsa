fd38e2e98423b91dc25bb792de29bde5a4a99fa4
bartr <bartr@microsoft.com>
2020-12-19 17:05:55 -0600

    updated coverage script

--- NewApp/ngsa-csharp/run-coverage.cmd
+++ NewApp/ngsa-csharp/run-coverage.cmd
@@ -5,23 +5,23 @@ dotnet tool install -g dotnet-reportgenerator-globaltool
 dotnet clean
 dotnet build
 
-start dotnet test Ngsa.App.Tests/Ngsa.App.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=../TestResults/ /p:MergeWith=../TestResults/coverage.json
-timeout /T 10 /nobreak
-
 start dotnet test Ngsa.DataService.Tests/Ngsa.DataService.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=../TestResults/  /p:MergeWith=../TestResults/coverage.json
+timeout /T 5 /nobreak
+
+start dotnet test Ngsa.App.Tests/Ngsa.App.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=../TestResults/ /p:MergeWith=../TestResults/coverage.json
 timeout /T 15 /nobreak
 
 dotnet test Ngsa.LodeRunner.Tests/Ngsa.LodeRunner.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=../TestResults/  /p:MergeWith=../TestResults/coverage.json
-timeout /T 25 /nobreak
+timeout /T 32 /nobreak
 
 set RUN_TEST_COVERAGE=
 dotnet test Ngsa.App.Tests/Ngsa.App.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=../TestResults/  /p:MergeWith=../TestResults/coverage.json
 dotnet test Ngsa.DataService.Tests/Ngsa.DataService.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=../TestResults/  /p:MergeWith=../TestResults/coverage.json
 dotnet test Ngsa.LodeRunner.Tests/Ngsa.LodeRunner.Tests.csproj /p:CollectCoverage=true /p:CoverletOutput=../TestResults/  /p:MergeWith=../TestResults/coverage.json /p:CoverletOutputFormat=opencover
 
-cd TestResults
-reportgenerator -reports:coverage.opencover.xml -targetdir:rpt -reporttypes:Html
-cd rpt
-index.html
+# cd TestResults
+# reportgenerator -reports:coverage.opencover.xml -targetdir:rpt -reporttypes:Html
+# cd rpt
+# index.html
 
-cd ../..
+# cd ../..
