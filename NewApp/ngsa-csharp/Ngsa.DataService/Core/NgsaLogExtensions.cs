0ffff828536767e0825cb110d896bebec796fe38
bartr <bartr@microsoft.com>
2021-01-06 18:18:46 -0600

    added log enrich

---
+++ NewApp/ngsa-csharp/Ngsa.DataService/Core/NgsaLogExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Ngsa.Middleware;
+
+namespace Ngsa.DataService
+{
+    public static class NgsaLogExtensions
+    {
+        public static NgsaLog EnrichLog(this NgsaLog log)
+        {
+            log.Data.Add("podType", App.PodType);
+            log.Data.Add("region", App.Region);
+            log.Data.Add("zone", App.Zone);
+
+            return log;
+        }
+    }
+}
